587a2783241b58732b91a4854171e5f40c2e07c7
Jesus Fco Cortes <jfcr@live.com>
2018-03-12 16:29:54 -0600

    Destacados Trabajos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,7 +131,59 @@
 
         </div>      
 
-        <div class="parallax1" style="background-image: url(img/index/parallax1.jpg)">            
+        <div class="parallax1" style="background-image: url(img/index/parallax1.jpg)">  
+        </div>
+
+        <div class="proyects">
+
+            <h1>Trabajos Destacados</h1>
+
+            <div class="proyects-container">
+
+                <div class="proyects-piece">
+                    <div>
+                        <h3>Amerigas</h3>
+                        <p>Desarrollo de pagina y aplicación web Amerigas 
+                        enfocada a la administración de ventas y proveedores
+                        de productos quimicos.</p>
+                    </div>
+                    <div>
+                        <img src="img/index/destacados1.jpg">
+                    </div>
+                </div>
+
+                <div class="proyects-piece">                
+                    <div>
+                        <img src="img/index/destacados2.jpg">
+                    </div>
+                    <div>
+                        <h3>Wolosky Gym</h3>
+                        <p>Desarrollo de pagina y aplicacion web de escritorio enfocada a la administración de pagos de alumnos y profesores mediante el acceso con sistema biometrico</p>
+                    </div>
+                </div>
+
+                <div class="proyects-piece">
+                    <div>
+                        <h3>RESIDENCIAL CHULAVISTA</h3>
+                        <p>Desarrollo de pagina y aplicación web Residencial Chulavista enfocada a la administracion de contratos de venta y pagos de casa de un fraccionamiento. 
+                        </p>
+                    </div>
+                    <div>
+                        <img src="img/index/destacados3.jpg">
+                    </div>
+                </div>
+
+                <div class="proyects-piece">
+                    
+                    <div>
+                        <img src="img/index/destacados4.jpg">
+                    </div>
+                    <div>
+                        <h3>GRUPO RUILO</h3>
+                        <p>Aplicación web enfocada a la administración de tareas a trabajadores mediante una interfaz estilo proveedor de correo, diseñado para generar bitacoras de progreso segun las nuevas entradas de los empleados por la tarea asignada.</p>
+                    </div>
+                </div>
+            </div>
         </div>
         
         <script src="{{ url('js/jquery-2.1.1.min.js') }}"></script>
